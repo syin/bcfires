3fc3eec5e4672bc7704eecbd22fd5af2b9c64c77
Justin Lee <justinlee0022@gmail.com>
2017-10-22 19:30:58 -0700

    Small bug fix and refactor

--- client/main.js
+++ client/main.js
@@ -1,46 +1,49 @@
+import path from 'path'
 import * as utils from './utils'
 import jsonfile from './output_simplified.json'
 
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 const fires = jsonfile.features
 
 app.use((request, response, next) => {
-    next();
-});
+  next()
+})
 
 app.use((err, request, response, next) => {
-    console.log(err);
-    response.status(500).send("error");
-});
+  console.log(err)
+  response.status(500).send('error')
+})
 
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + "/map.html");
-});
+  response.sendFile(path.join(__dirname, '/map.html'))
+})
 
-app.get('/map.js', (request, response) => {
-    response.sendFile(__dirname + "/map.js");
-});
+// app.get('/map.js', (request, response) => {
+//   response.sendFile(__dirname + '/map.js')
+// })
+
+app.use(express.static('public'))
 
 app.get('/output_simplified.json', (request, response) => {
-    response.sendFile(__dirname + "/output_simplified.json");
-});
+  response.sendFile(path.join(__dirname, '/output_simplified.json'))
+})
 
 app.get('/bcmap.geojson', (request, response) => {
-    response.sendFile(__dirname + "/bcmap.geojson")
+  response.sendFile(path.join(__dirname, '/bcmap.geojson'))
 })
 
 app.get('/fires/:year', (req, res) => {
-    const year = req.params.year
-    const firesInYear = utils.filterFiresByYear(year)(fires)
-    res.send(firesInYear)
+  const year = req.params.year
+  const firesInYear = utils.filterFiresByYear(year)(fires)
+  res.send(firesInYear)
 })
 
 app.listen(port, (err) => {
-    if (err) {
-        return console.log("error", err);
-    }
-    console.log("server is listening on ", port);
-});
+  if (err) {
+    return console.log('error', err)
+  }
+  console.log('server is listening on ', port)
+})
--- client/utils.js
+++ client/utils.js
@@ -2,7 +2,6 @@ import R from 'ramda'
 
 const filterFiresByYear = (year) => R.filter(x => Number(x.properties.FIRE_YEAR) === Number(year))
 
-
 export {
     filterFiresByYear
 }
