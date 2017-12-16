e894c8789b2c7e40e7f87689e6210adc31e1bdf1
Justin Lee <justin.lee@ubc.ca>
2017-12-16 12:36:49 -0800

    fixed async bug (NOT A RACE CONDITION!!!)

--- client/public/map.js
+++ client/public/map.js
@@ -4,9 +4,9 @@ const getFiresByYear = (year) => fetch('/fires/' + year).then(response => respon
 
 const humanizeHa = (input) => {
   if (input >= 10000) {
-    return (input/1000).toFixed(1) + " kha"
+    return (input / 1000).toFixed(1) + ' kha'
   } else {
-    return input + " ha"
+    return input + ' ha'
   }
 }
 
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
       .scale(2200))
 
   const svg = d3.select('#map')
-    .attr('viewBox', "0 0 " + width + " " + height)
-    .attr('preserveAspectRatio', "xMidYMid meet")
+    .attr('viewBox', '0 0 ' + width + ' ' + height)
+    .attr('preserveAspectRatio', 'xMidYMid meet')
 
   const drawGraph = (json, fill, id) => {
     svg.append('g')
@@ -39,8 +39,8 @@ document.addEventListener('DOMContentLoaded', function () {
       .append('path')
       .attr('d', path)
       .style('fill', fill)
-      .on("mouseover", function(d) {
-        if (d.type === "Feature") {
+      .on('mouseover', function (d) {
+        if (d.type === 'Feature') {
           // calculate midpoint of polygon
           const bbox = this.getBBox()
           const cx = bbox.x + bbox.width / 2
@@ -77,7 +77,6 @@ document.addEventListener('DOMContentLoaded', function () {
       .remove()
 
     d3.selectAll('#causeBar g')
-      .transition()
       .remove()
   }
 
@@ -110,65 +109,64 @@ document.addEventListener('DOMContentLoaded', function () {
       return elem.properties.SIZE_HA
     })
 
-    console.log("areas", areas)
+    console.log('areas', areas)
 
     const max = d3.max(areas)
     const min = d3.min(areas)
     const x = d3.scaleLinear()
       .domain([min, max])
-      .range([0, width]);
+      .range([0, width])
 
-    const formatCount = d3.format(",.0f");
+    const formatCount = d3.format(',.0f')
 
     const bins = d3.histogram()
       .domain(x.domain())
-      .thresholds(x.ticks(10))
-      (areas)
+      .thresholds(x.ticks(10))(areas)
 
-    const yMax = d3.max(bins, function(d){return d.length})
-    const yMin = d3.min(bins, function(d){return d.length})
+    const yMax = d3.max(bins, function (d) { return d.length })
+    const yMin = d3.min(bins, function (d) { return d.length })
     const colorScale = d3.scaleLinear()
       .domain([yMin, yMax])
       .range([d3.rgb(colours.red).brighter(), d3.rgb(colours.red).darker()])
 
     const y = d3.scaleLinear()
-      .domain([0, d3.max(bins, function(d) { return d.length })])
+      .domain([0, d3.max(bins, function (d) { return d.length })])
       .range([height, 0])
 
     const svgHist = d3.select('#areaHist')
-      .attr('viewBox', "0 -10 " + width + " " + (height + 40))
-      .attr('preserveAspectRatio', "xMidYMid meet")
+      .attr('viewBox', '0 -10 ' + width + ' ' + (height + 40))
+      .attr('preserveAspectRatio', 'xMidYMid meet')
 
-    const g = svgHist.append("g")
+    const g = svgHist.append('g')
 
-    const bar = g.selectAll(".bar")
+    const bar = g.selectAll('.bar')
       .data(bins)
-      .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")" })
+      .enter().append('g')
+      .attr('class', 'bar')
+      .attr('transform', function (d) { return 'translate(' + x(d.x0) + ',' + y(d.length) + ')' })
 
-    bar.append("rect")
-      .attr("x", 1)
-      .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-      .attr("height", function(d) { return height - y(d.length) })
-      .attr("fill", function(d) { return colorScale(d.length) })
+    bar.append('rect')
+      .attr('x', 1)
+      .attr('width', x(bins[0].x1) - x(bins[0].x0) - 1)
+      .attr('height', function (d) { return height - y(d.length) })
+      .attr('fill', function (d) { return colorScale(d.length) })
 
-    bar.append("text")
-      .attr("dy", ".75em")
-      .attr("y", -10)
-      .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-      .attr("text-anchor", "middle")
-      .text(function(d) { return formatCount(d.length) })
+    bar.append('text')
+      .attr('dy', '.75em')
+      .attr('y', -10)
+      .attr('x', (x(bins[0].x1) - x(bins[0].x0)) / 2)
+      .attr('text-anchor', 'middle')
+      .text(function (d) { return formatCount(d.length) })
 
-    g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
+    g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(x)
-              .tickFormat(d3.format(".0s")))
-
+              .tickFormat(d3.format('.0s')))
   }
 
-  drawCauseBarChart = (data) => {
+  const drawCauseBarChart = (data) => {
+    console.log('in drawCause', data)
     // Adapted from https://bl.ocks.org/hrecht/f84012ee860cb4da66331f18d588eee3
     const width = 300
     const height = 300
@@ -186,67 +184,66 @@ document.addEventListener('DOMContentLoaded', function () {
     }, {})
 
     const cause_list = Object.keys(cause_dict).reduce((acc, key) => {
-      acc.push({"name": key, "value": cause_dict[key]})
+      acc.push({'name': key, 'value': cause_dict[key]})
       return acc
     }, [])
 
     console.log(cause_list)
 
     const svgBar = d3.select('#causeBar')
-      .attr('viewBox', "-50 0 500 " + height)
-      .attr('preserveAspectRatio', "xMidYMid meet")
+      .attr('viewBox', '-50 0 500 ' + height)
+      .attr('preserveAspectRatio', 'xMidYMid meet')
 
     const x = d3.scaleLinear()
       .range([0, width])
       .domain([0, d3.max(cause_list, function (d) {
-        return d.value;
+        return d.value
       })])
 
     const y = d3.scaleBand()
-      .range([height, 0], .1)
+      .range([height, 0], 0.1)
       .domain(cause_list.map(function (d) {
-        return d.name;
+        return d.name
       }))
 
     const yAxis = d3.axisLeft(y)
-      //no tick marks
+      // no tick marks
       .tickSize(0)
 
-    const gy = svgBar.append("g")
-      .attr("class", "y axis")
+    const gy = svgBar.append('g')
+      .attr('class', 'y axis')
       .call(yAxis)
 
-    const bars = svgBar.selectAll(".bar")
+    const bars = svgBar.selectAll('.bar')
       .data(cause_list)
       .enter()
-      .append("g")
+      .append('g')
 
-    //append rects
-    bars.append("rect")
-      .attr("class", "bar")
-      .attr("y", function (d) {
-          return y(d.name)
+    // append rects
+    bars.append('rect')
+      .attr('class', 'bar')
+      .attr('y', function (d) {
+        return y(d.name)
       })
-      .attr("height", y.bandwidth())
-      .attr("x", 0)
-      .attr("width", function(d) {
+      .attr('height', y.bandwidth())
+      .attr('x', 0)
+      .attr('width', function (d) {
         return x(d.value)
       })
 
-    bars.append("text")
-      .attr("class", "label")
-      //y position of the label is halfway down the bar
-      .attr("y", function (d) {
+    bars.append('text')
+      .attr('class', 'label')
+      // y position of the label is halfway down the bar
+      .attr('y', function (d) {
         return y(d.name) + y.bandwidth() / 2 + 4
       })
-      //x position is 3 pixels to the right of the bar
-      .attr("x", function (d) {
+      // x position is 3 pixels to the right of the bar
+      .attr('x', function (d) {
         return x(d.value) + 3
       })
       .text(function (d) {
         return d.value
       })
-
   }
 
   const render = (data) => {
@@ -279,11 +276,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const leftMargin = 0.0530483
     const rightMargin = 88.3731
 
-    const yearLabel = document.getElementsByClassName("irs-single")[0]
+    const yearLabel = document.getElementsByClassName('irs-single')[0]
     const position = parseFloat(yearLabel.style.left)
 
     const format = (inputFloat) => {
-      return inputFloat.toString() + "%"
+      return inputFloat.toString() + '%'
     }
 
     if (position > rightMargin) {
